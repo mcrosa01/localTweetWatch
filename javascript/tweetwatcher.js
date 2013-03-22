8ecded22e871025013a54a91218698ff3bc5a384
Jake Nowosatka <nowoja01@luther.edu>
2013-03-22 09:52:55 -0500

    Made getTweets function accept appropriate parameters, allowing it to be dynamic.

--- javascript/tweetwatcher.js
+++ javascript/tweetwatcher.js
@@ -1,9 +1,17 @@
 var since_id = "";
 
-function getTweets() {
+
+/**
+ * Takes a Longitude, latitude and radius of the Tweet search and adds all the 
+ * tweets returned by the twitter search api in the div element called 'twitterfeed'
+ *  
+ * @param {Number} longitude - A double precision number
+ * @param {Number} latitude - A double precision number
+ * @param {Number} radius - An integer
+ */
+function getTweets(longitude, latitude, radius) {
 	//Get the Long, Lat, and radius
-	debugger;
-	var coords = document.getElementById("coords").value;
+	var coords = longitude + "," + latitude + "," + radius + "mi"
 	if (coords === "") {
 		coords = "37.781157,-122.398720,1mi";
 	}
@@ -16,7 +24,7 @@ function getTweets() {
 		var url = "http://search.twitter.com/search.json?geocode=" + coords + "&callback=?";
 	}
 	$.getJSON(url, function(json) {
-		console.log(json);
+		//console.log(json);
 		//Get the twitter feed div
 		var tfeed = document.getElementById("twitterfeed");
 		//Clear current tweets
